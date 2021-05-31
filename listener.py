8d9cf0b8e09193f4983b0cfdd21ce629733d46af
Abdulrahman Ajlouni <ajlouni2000@gmail.com>
2021-05-31 12:28:29 +0300

    rename server.py to listener.py

--- listener.py
+++ listener.py
@@ -4,24 +4,24 @@ import sys
 from buffered_socket import BufferedSocket
 
 
-class Server:
+class Listener:
 
-    def __init__(self, host, port, handler):
+    def __init__(self, host, port, client_handler):
         self.host = host
         self.port = port
-        self.handler = handler
+        self.cliend_handler = client_handler
         self.sock = BufferedSocket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((host, port))
 
-    def run(self):
-        print("Server running on {}:{}...".format(self.host, self.port))
+    def listen(self):
+        print("Server listening on {}:{}...".format(self.host, self.port))
         self.sock.listen()
         while True:
             conn, addr = self.sock.accept()
             print(f"Connection from {addr[0]}:{addr[1]} established!",
                   file=sys.stderr)
 
-            t = threading.Thread(target=self.handler, args=[conn, addr])
+            t = threading.Thread(target=self.cliend_handler, args=[conn, addr])
             t.start()
 
     def close(self):
