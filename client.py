6aa9e4b056605fb2c08fcd026c2bb5f6e7747f05
Abdulrahman Ajlouni <ajlouni2000@gmail.com>
2021-05-31 23:15:42 +0300

    proper freeing of resources

--- client.py
+++ client.py
@@ -1,36 +1,52 @@
 #!/usr/bin/env python
 import socket
+import signal
 import json
 from buffered_socket import BufferedSocket
 
+def signal_handler(sig, frame):
+    return
+
 
 def main():
+    
+    # This prevents the client from exiting with ^C, to free resources properly.
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Load ip and port
     with open("config.json", "r") as f:
         confjson = json.load(f)
 
+    # Client socket
     sock = BufferedSocket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((confjson["HOST"], confjson["PORT"]))
 
+    # Remote procedures
     methods = {
         "1": "sum",
         "2": "sort",
         "3": "max",
         "4": "transpose",
         "5": "count",
-        "q": "quit"
+        "d": "disconnect"
     }
 
+    # User prompt
     prompt = "Select an operation:\n"
     for key, value in methods.items():
         prompt += key + ": " + value + "\n"
-
-    while (method_id := input(prompt)) != "q":
+    
+    # Communication loop
+    while True:
+        method_id = input(prompt).lower()
         if method_id not in methods:
             continue
 
         method = methods[method_id]
-
         sock.send(method.encode("utf-8"))
+
+        if method_id == "d":
+            break
 
         response = sock.recv().decode("utf-8")
         response = json.loads(response)
